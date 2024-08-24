e0a19a4ac495aa06dcf895cbfa61b9a89e54d5f9
t0sh1t0sh1 <tosh1syog1@yahoo.co.jp>
2024-08-24 12:35:46 +0900

    countボタンの追加

--- js/main.js
+++ js/main.js
@@ -31,3 +31,57 @@ $(function(){
   // });
 
 });
+
+
+// カウントボタン
+const number = document.querySelector('.count-status');
+const plusButton1 = document.querySelector(".add1");
+const plusButton5 = document.querySelector(".add5");
+const plusButton10 = document.querySelector(".add10");
+const minusButton1 = document.querySelector(".sub1");
+const minusButton5 = document.querySelector(".sub5");
+const minusButton10 = document.querySelector(".sub10");
+const resetButton = document.querySelector(".reset");
+
+let count = 0;
+
+plusButton1.addEventListener("click", () => {
+  count++;
+  number.innerHTML = count;
+});
+
+plusButton5.addEventListener("click", () => {
+  count += 5;
+  number.innerHTML = count;
+});
+
+plusButton10.addEventListener("click", () => {
+  count += 10;
+  number.innerHTML = count;
+});
+
+minusButton1.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    number.innerHTML = count;
+  }
+})
+
+minusButton5.addEventListener("click", () => {
+  if (count > 4) {
+    count -= 5;
+    number.innerHTML = count;
+  }
+})
+
+minusButton10.addEventListener("click", () => {
+  if (count > 9) {
+    count -= 10;
+    number.innerHTML = count;
+  }
+})
+
+resetButton.addEventListener("click", () => {
+  count = 0;
+  number.innerHTML = count;
+});
